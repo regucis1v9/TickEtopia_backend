c5398a4a600f723ae268429d84f9ce6975889c08
Diablo785 <kalugaimants@gmail.com>
2025-03-12 22:45:09 +0200

    idk

--- database/migrations/2025_03_12_000010_create_users_table.php
+++ database/migrations/2025_03_12_000010_create_users_table.php
@@ -8,20 +8,32 @@ return new class extends Migration
 {
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('remember_token')->nullable();
-            $table->string('role')->default('user');
-            $table->timestamps(0);
-        });
+        if (!Schema::hasTable('users')) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('email');
+                $table->timestamp('email_verified_at')->nullable();
+                $table->string('password');
+                $table->string('remember_token')->nullable();
+                $table->string('role')->default('user');
+                $table->timestamps(0);
+            });
+        } else {
+            if (!Schema::hasColumn('users', 'role')) {
+                Schema::table('users', function (Blueprint $table) {
+                    $table->string('role')->default('user')->after('remember_token');
+                });
+            }
+        }
     }
     
     public function down()
     {
-        Schema::dropIfExists('users');
+        if (Schema::hasColumn('users', 'role')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('role');
+            });
+        }
     }
 };

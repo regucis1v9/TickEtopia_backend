e39728252d76862c49f338047d6ab86470c38afe
Diablo785 <kalugaimants@gmail.com>
2025-05-12 20:19:43 +0300

    Pabeigts

--- app/Models/TicketHistory.php
+++ app/Models/TicketHistory.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TicketHistory extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'ticket_id',
         'user_id',
@@ -15,10 +18,22 @@ class TicketHistory extends Model
 
     public $timestamps = true;
 
-    public function ticket() {
+    public function ticket()
+    {
         return $this->belongsTo(Ticket::class);
     }
 
+    /**
+     * Get the user associated with the history record.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the status associated with the history record.
+     */
     public function status()
     {
         return $this->belongsTo(TicketStatus::class, 'status_id');
